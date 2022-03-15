5828ea5effc323671c20fac7068a09215dd47b45
Faisal Amir <faisalamircs@gmail.com>
2022-03-15 18:48:30 +0700

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,4 @@ repositories {
 dependencies {
     // library open-build-src
     implementation("com.github.frogobox:open-build-src:1.0.6")
-
-    // library frogo-build-src
-    implementation("com.github.frogobox:frogo-build-src:1.0.8")
 }
