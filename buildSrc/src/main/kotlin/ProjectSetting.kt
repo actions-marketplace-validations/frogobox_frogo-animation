c90b9e6df822d518a10880da711266dafa381531
Faisal Amir <faisalamircs@gmail.com>
2022-03-15 18:35:22 +0700

    Update ProjectSetting.kt

--- buildSrc/src/main/kotlin/ProjectSetting.kt
+++ buildSrc/src/main/kotlin/ProjectSetting.kt
@@ -13,52 +13,51 @@
 
 object ProjectSetting {
 
-    // ---------------------------------------------------------------------------------------------
-
-    // Project settings
-    const val NAME_APP = "Frogo Piano Tiles"
+    const val NAME_APP = "Frogo SDK"
 
     const val APP_DOMAIN = "com"
     const val APP_PLAY_CONSOLE = "frogobox"
-    const val APP_NAME = "pianogames"
+    const val APP_NAME = "appsdk"
 
-    const val VERSION_MAJOR = 1
+    const val LIBRARY_NAME_SDK = "library"
+    const val LIBRARY_NAME_CORE_SDK = "coresdk"
+
+    const val MODULE_NAME_SDK = "frogolibrary"
+    const val MODULE_NAME_CORE_SDK = "frogocoresdk"
+
+    // ---------------------------------------------------------------------------------------------
+
+    const val VERSION_MAJOR = 0
     const val VERSION_MINOR = 0
-    const val VERSION_PATCH = 0
+    const val VERSION_PATCH = 1
 
     // ---------------------------------------------------------------------------------------------
 
-    // Key Store
-    const val PLAYSTORE_STORE_FILE = "frogoboxmedia.jks"
-    const val PLAYSTORE_STORE_PASSWORD = "amirisback"
-    const val PLAYSTORE_KEY_ALIAS = "frogoisback"
-    const val PLAYSTORE_KEY_PASSWORD = "amirisback"
-
-    // ---------------------------------------------------------------------------------------------
-    // Default Setting - Do Not Change
-    // ---------------------------------------------------------------------------------------------
-
-    const val PROJECT_COMPILE_SDK = 31
     const val PROJECT_MIN_SDK = 21
+    const val PROJECT_COMPILE_SDK = 31
     const val PROJECT_TARGET_SDK = PROJECT_COMPILE_SDK
 
     // ---------------------------------------------------------------------------------------------
 
-    const val PROJECT_APP_ID = "$APP_DOMAIN.$APP_PLAY_CONSOLE.$APP_NAME"
+    const val BASE_PACAKGE_NAME = "$APP_DOMAIN.$APP_PLAY_CONSOLE"
+
+    const val PROJECT_APP_ID = "$BASE_PACAKGE_NAME.$APP_NAME"
+    const val PROJECT_LIB_ID_SDK = "$BASE_PACAKGE_NAME.$LIBRARY_NAME_SDK"
+    const val PROJECT_LIB_ID_CORE_SDK = "$BASE_PACAKGE_NAME.$LIBRARY_NAME_CORE_SDK"
+
     const val PROJECT_VERSION_CODE = (VERSION_MAJOR * 100) + (VERSION_MINOR * 10) + (VERSION_PATCH * 1)
     const val PROJECT_VERSION_NAME = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
 
+    val NAME_APK = NAME_APP.toLowerCase().replace(" ", "-")
+
+    val NAME_DB = NAME_APP.toLowerCase().replace(" ", "_")
+    val DB = "\"$NAME_DB.db\""
+
     // ---------------------------------------------------------------------------------------------
 
-    // Declaration apps name debug mode
-    const val DEBUG_ATTRIBUTE = "Development"
-    const val NAME_APP_DEBUG = "$NAME_APP $DEBUG_ATTRIBUTE"
-
-    // Constant Variable
-    val NAME_APK = NAME_APP.toLowerCase().replace(" ", "-")
-    val NAME_SETTING_LOWERCASE = NAME_APP.toLowerCase().replace(" ", "_")
-    val NAME_SETTING_UPERCASE = NAME_APP.toUpperCase().replace(" ", "_")
-    val DB = "db_$NAME_SETTING_LOWERCASE.db"
-    val PREF_NAME = "PREF_GAME_$NAME_SETTING_UPERCASE"
+    const val PLAYSTORE_STORE_FILE = "frogoboxmedia.jks"
+    const val PLAYSTORE_STORE_PASSWORD = "amirisback"
+    const val PLAYSTORE_KEY_ALIAS = "frogoisback"
+    const val PLAYSTORE_KEY_PASSWORD = "amirisback"
 
 }
